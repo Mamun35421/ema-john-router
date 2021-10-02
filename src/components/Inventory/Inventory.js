d43fcb2a4ab1126a3d304eae67bd7ff512616fc1
Mamun35421 <mamunorrashid.me@gmail.com>
2021-10-02 19:11:27 +0600

    updated

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-           <h1>Well come inventory</h1> 
+           <h1>Well come inventory page</h1> 
         </div>
     );
 };
